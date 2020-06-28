df82f1d3b0927f06931faf5ab9f5702619bb4724
Karandeep Singh <karandeep041998@gmail.com>
2020-06-29 03:49:34 +0530

    UI changes

--- app/src/main/java/com/example/androidtutor/AndroidImagesAndMedia/ImageButton/ImageButtonExample.java
+++ app/src/main/java/com/example/androidtutor/AndroidImagesAndMedia/ImageButton/ImageButtonExample.java
@@ -4,21 +4,31 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
 import com.example.androidtutor.R;
-
 public class ImageButtonExample extends Fragment {
+    private ImageButton imgbt;
     public ImageButtonExample() {
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_imagebutton_example, container, false);
+        View v= inflater.inflate(R.layout.fragment_imagebutton_example, container, false);
+        imgbt=v.findViewById(R.id.imgbt);
+        imgbt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"ImageButton was Clicked!",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        return v;
     }
 }
